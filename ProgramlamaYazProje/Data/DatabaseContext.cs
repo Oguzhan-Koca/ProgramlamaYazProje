50cc5293d51dcb7207f6e1944cb20d9c8578635c
Oguzhan-Koca <83463216+Oguzhan-Koca@users.noreply.github.com>
2023-07-31 22:59:39 +0300

    Add project files.

---
+++ ProgramlamaYazProje/Data/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProgramlamaYazProje.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramlamaYazProje.Data
+{
+    public class DatabaseContext : DbContext
+    {
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
